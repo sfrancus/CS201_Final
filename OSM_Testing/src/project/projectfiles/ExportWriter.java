c4c21c69a1b069b45f25124779b085973d2c23ff
sh0rtfuse <rdam@usc.edu>
2014-04-13 03:04:19 -0700

    Testing
    [ADDED] Testing folder with White and Black Box Testing
    [ADDED] README testing
    Submitted code should included the testing folder.

---
+++ OSM_Testing/src/project/projectfiles/ExportWriter.java
@@ -0,0 +1,53 @@
+package project.projectfiles;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+
+public class ExportWriter {
+	private Object [][] data;
+	
+	public ExportWriter(Object[][] data) {
+		this.data = data;
+	}
+	
+	public void export(JComponent parent) {
+		try {
+			//User Input
+			String fileName = JOptionPane.showInputDialog(parent, "Please enter desired Filename.", 
+				"Export to .CSV", JOptionPane.QUESTION_MESSAGE
+			);
+			fileName += ".csv";
+			
+			if(!fileName.equals("null.csv")) {
+		         //write file
+	            FileWriter fw = new FileWriter(fileName);
+	            PrintWriter pw = new PrintWriter(fw);
+	            
+	            //===Output Format===//
+	            // Column 1, Column 2
+	            // data, data
+	            //===================//
+	            //loops over rows of col1
+	            for(int i = 0; i < data.length; i++) {
+	                pw.print(data[i][0] + ",");
+	                pw.print(data[i][1]);
+	                pw.println();
+	            }
+	            
+	            pw.flush();
+	            pw.close();
+	            fw.close();
+			}
+		}
+		catch(NullPointerException npe) {
+			System.out.println("npe: " + npe.getMessage());
+		}
+		catch(IOException ioe) {
+			System.out.println("ioe: " + ioe.getMessage());
+		}
+	}
+}
